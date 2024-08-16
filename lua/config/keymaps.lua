cad692bc328c3f21d40ed66239707d1e827c5058
feri <feri@Xubuntu.myguest.virtualbox.org>
2024-08-16 10:17:39 -0700

    Initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+vim.keymap.set("", "<Space>", "<Nop>")
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>dl", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])

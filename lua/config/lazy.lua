63fecfbe580f6c463e138fee7134937109229b8d
Feri <farzad.dianat@gmail.com>
2024-08-31 08:09:25 -0700

    reduce number of space for indentaion

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,9 +27,9 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.opt.nu = true
 vim.opt.rnu = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true

7d2c3133d082003d33329337912bf2470fd7e64b
Feri <farzad.dianat@gmail.com>
2024-08-31 19:07:53 -0700

    Add a completion plugin for neovim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,27 +1,27 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver" },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lsp_config = require("lspconfig")
-            lsp_config.lua_ls.setup({})
-            lsp_config.tsserver.setup({})
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lsp_config = require("lspconfig")
+      lsp_config.lua_ls.setup({})
+      lsp_config.tsserver.setup({})
 
-            vim.keymap.set({ "n", "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-        end,
-    },
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+    end,
+  },
 }
